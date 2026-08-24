4b0448ccf87c4130d4effb1504537a09b96a01e5
Bryan Aquino <97378754+BryanAquino02@users.noreply.github.com>
2026-08-24 15:02:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Completa esto con los datos de tu proyecto Supabase
 // (Settings → API en el dashboard de Supabase)
 window.SUPABASE_URL = "https://finoqbcoytpoylxdosry.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpbm9xYmNveXRwb3lseGRvc3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1NzU1NTksImV4cCI6MjEwMzE1MTU1OX0.bu7r-NiiLoliKH2ZrukkSRsnKLI8FjdO9jTAA3wxHwUA";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpbm9xYmNveXRwb3lseGRvc3J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1NzU1NTksImV4cCI6MjEwMzE1MTU1OX0.bu7r-NiiLoliKH2ZrukkSRsnKLI8FjdO9jTAA3wxHwU";
